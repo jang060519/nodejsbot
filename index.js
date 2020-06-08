8be21e636d69a08c6e88066ca6713963cd19a680
jang060519 <jang060519@gmail.com>
2020-06-08 17:22:10 +0900

    add

--- index.js
+++ index.js
@@ -36,6 +36,36 @@ client.on('message', (message) => {
     return message.reply('안녕하세요');
   }
 
+  if(message.content == '!si') {
+    let embed = new Discord.RichEmbed()
+    let img = 'https://cdn.discordapp.com/attachments/718521409843888220/719466063544451072/bot_img.PNG';
+    var duration = moment.duration(client.uptime).format(" D [일], H [시간], m [분], s [초]");
+    embed.setColor('#186de6')
+    embed.setAuthor('server info of Melode 봇', img)
+    embed.setFooter(`Melode 봇❤️`)
+    embed.addBlankField()
+    embed.addField('RAM usage',    `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true);
+    embed.addField('running time', `${duration}`, true);
+    embed.addField('user',         `${client.users.size.toLocaleString()}`, true);
+    embed.addField('server',       `${client.guilds.size.toLocaleString()}`, true);
+    // embed.addField('channel',      `${client.channels.size.toLocaleString()}`, true);
+    embed.addField('Discord.js',   `v${Discord.version}`, true);
+    embed.addField('Node',         `${process.version}`, true);
+
+    let arr = client.guilds.array();
+    let list = '';
+    list = `\`\`\`css\n`;
+
+    for(let i=0;i<arr.length;i++) {
+      // list += `${arr[i].name} - ${arr[i].id}\n`
+      list += `${arr[i].name}\n`
+    }
+    list += `\`\`\`\n`
+    embed.addField('list:',        `${list}`);
+
+    embed.setTimestamp()
+    message.channel.send(embed);
+  }
 
   if(message.content == '/who 녤쁨') {     //부마스터 정보(녤쁨님)
     let img = 'https://cdn.discordapp.com/attachments/718521409843888220/718542239881756783/unknown.png';
